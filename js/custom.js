321b2a6ec5d12bdbb5900709a344baa08ffd11f0
lazySia <tldk9640@naver.com>
2022-01-03 15:38:49 +0900

    box2

--- js/custom.js
+++ js/custom.js
@@ -8,5 +8,8 @@ $(document).ready(function (e) {
     $(".btnMenu").fadeIn();
     $("nav").removeClass("on");
     $("section").removeClass("on");
+    var index = $(this).index();
+    $("section>div").removeClass("on");
+    $("section>div").eq(index).addClass("on");
   });
 });
